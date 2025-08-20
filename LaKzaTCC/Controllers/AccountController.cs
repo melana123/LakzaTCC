684372be09c37aac3843bc93798951c111bfda66
melana123 <melana.etec@gmail.com>
2025-08-20 10:40:24 -0300

    terminei a poag 9 do gstore autenticação

---
+++ LaKzaTCC/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+using LaKzaTCC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LaKzaTCC.Controllers;
+public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<Usuario> _signInManager;
+        private readonly UserManager<Usuario> _userManager;
+        private readonly IWebHostEnvironment _host;
+
+        public AccountController(
+            ILogger<AccountController> logger,
+            SignInManager<Usuario> signInManager,
+            UserManager<Usuario> userManager,
+            IWebHostEnvironment host
+            )
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _host = host;
+        }
+
+    }
