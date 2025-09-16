37ed06c1346af3d9f2891b7aac84e1c5fc50d1c1
melana123 <melana.etec@gmail.com>
2025-09-16 11:42:58 -0300

    ta indo +-, passei o carrossel para o projeto e arrumei o homecontroller q o gallo pediu

--- LaKzaTCC/Controllers/HomeController.cs
+++ LaKzaTCC/Controllers/HomeController.cs
@@ -1,21 +1,52 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using LaKzaTCC.Models;
+using LaKzaTCC.Data;
+using Microsoft.EntityFrameworkCore;
+using LaKzaTCC.ViewModels;
 
 namespace LaKzaTCC.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AppDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
     {
-        return View();
+        List<Produto> produtos = _db.Produtos
+            .Where(p => p.Destaque)
+            .ToList();
+        return View(produtos);
+    }
+
+    public IActionResult Produto(int id)
+    {
+        Produto produto = _db.Produtos
+            .Where(p => p.Id == id)
+            .Include(p => p.Categoria)
+            .Include(p => p.ArquivoFoto)
+            .SingleOrDefault();
+        
+        List<Produto> semelhantes = _db.Produtos
+            .Where(p => p.Id != id && p.CategoriaId == produto.CategoriaId)
+            .Include(p => p.Categoria)
+            .Include(p => p.ArquivoFoto)
+            .Take(4)
+            .ToList();
+        
+        ProdutoVM produtoVM = new() {
+            Produto = produto,
+            Semelhantes = semelhantes
+        };
+        
+        return View(produtoVM);
     }
 
     public IActionResult Privacy()
